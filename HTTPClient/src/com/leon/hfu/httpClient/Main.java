5417332c0a19bab0a5da29b009163f2feea4604a
Stefan <Stefan_Hahn1@gmx.net>
2015-04-10 01:37:45 +0200

    Added test use case of HTTPRequest class

--- HTTPClient/src/com/leon/hfu/httpClient/Main.java
+++ HTTPClient/src/com/leon/hfu/httpClient/Main.java
@@ -2,6 +2,9 @@ package com.leon.hfu.httpClient;
 
 import com.leon.hfu.util.commandLine.CommandLineExecutor;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 /**
  *
  *
@@ -21,6 +24,19 @@ public class Main {
 	public static void main(String[] args) {
 		program = new HTTPClientProgram();
 
-		CommandLineExecutor.run(program, args);
+		// CommandLineExecutor.run(program, args);
+
+		try {
+			HTTPRequest req = (new HTTPRequest("http://pr0gramm.com/")).execute().parse();
+
+			System.out.println(req.getRawResponse());
+		}
+		catch (MalformedURLException e) {
+			System.err.println("Invalid URL!");
+		}
+		catch (IOException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
