032dd962f403e404e9a934b2e7b30d348cae528f
Stefan <Stefan_Hahn1@gmx.net>
2015-05-05 20:33:15 +0200

    Added basic user handling from session

--- TicketSale/src/com/leon/hfu/web/ticketSale/Core.java
+++ TicketSale/src/com/leon/hfu/web/ticketSale/Core.java
@@ -1,14 +1,16 @@
 package com.leon.hfu.web.ticketSale;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Stefan on 01.05.2015.
  */
 public final class Core {
 	private static Core instance = null;
 
-	private Core() {
-
-	}
+	private Core() { }
 
 	public static Core getInstance() {
 		if (Core.instance == null) {
@@ -16,5 +18,37 @@ public final class Core {
 		}
 
 		return Core.instance;
+	}
+
+	public void initSession(HttpServletRequest request, HttpServletResponse response) {
+		Cookie[] cookies = request.getCookies();
+		int userID = -1;
+		String passwordHash = null;
+		User user;
+
+		if (cookies != null) {
+			for (Cookie cookie: cookies) {
+				if (cookie.getName().equals("ticketsale_userID")) {
+					userID = Integer.parseInt(cookie.getValue(), 10);
+				}
+
+				if (cookie.getName().equals("ticketsale_passwordHash")) {
+					passwordHash = cookie.getValue();
+				}
+			}
+		}
+
+		if (request.getSession(true).isNew() || (request.getSession().getAttribute("user") == null)) {
+			if (userID > 0) {
+				// TODO: check login information
+				user = User.getUserByUserID(userID);
+			}
+			else {
+				user = User.DEFAULT_USER;
+			}
+
+			request.getSession().setAttribute("user", user);
+			response.addCookie(new Cookie("ticketsale_userID", Integer.toString(userID)));
+		}
 	}
 }
