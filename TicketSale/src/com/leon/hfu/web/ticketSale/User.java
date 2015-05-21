96e728e64c081d9117308d042e7a78c1f455f3b0
Stefan <Stefan_Hahn1@gmx.net>
2015-05-21 09:10:50 +0200

    Added new methods to get users by username

--- TicketSale/src/com/leon/hfu/web/ticketSale/User.java
+++ TicketSale/src/com/leon/hfu/web/ticketSale/User.java
@@ -61,4 +61,12 @@ public class User {
 
 		return UserHandler.getInstance().getUsersByID(new int[] {userID})[0];
 	}
+
+	public static User getUserByUsername(String username) throws NoSuchUserException {
+		if ((username == null) || (username.length() == 0)) {
+			throw new IllegalArgumentException();
+		}
+
+		return UserHandler.getInstance().getUsersByName(new String[] {username})[0];
+	}
 }
