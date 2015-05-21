84f6bfb79ea25ea88be08dbf1d5ab03129a2ee78
Stefan <Stefan_Hahn1@gmx.net>
2015-05-21 12:16:09 +0200

    Implemented whole handleSeat() function

--- TicketSale/web/js/main.js
+++ TicketSale/web/js/main.js
@@ -5,7 +5,22 @@ $(document).ready(function() {
 });
 
 function handleSeat(seatNode) {
-	if (seatNode.hasClass('seat-free')) {
-		seatNode.addClass('seat-marked');
+	if (seatNode.hasClass('seat-free') || seatNode.hasClass('seat-own')) {
+		var isMarked = seatNode.hasClass('seat-marked');
+		var seatID = parseInt(seatNode.data('id'), 10);
+
+		if (isMarked) {
+			$('#ticketForm #seat-form-input-' + seatID.toString(10)).remove();
+		}
+		else {
+			var inputElement = $('<input type="hidden" name="seatIDs[]" />');
+			inputElement.attr({
+				id: 'seat-form-input-' + seatID.toString(10),
+				value: seatID.toString(10)
+			});
+			$('#ticketForm').append(inputElement);
+		}
+
+		seatNode.toggleClass('seat-marked');
 	}
 }
